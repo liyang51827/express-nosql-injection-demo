de006602e9a7681e6000217ffde20fd4f35a7b27
liyang51827 <121928063+liyang51827@users.noreply.github.com>
2023-06-05 12:38:28 +0800

    make repo for dev.to post - nosql injection

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
 # express-nosql-injection-demo
+
 Technical Demo for Testing NoSQL Injection on Express MongoDB Server
+
+![NoSQL Injection Attack](diagram.jpg)
+
+&copy; 2023 liyang51827@gmail.com
+
+All Rights Reserved.
---
+++ app.js
@@ -0,0 +1,35 @@
+require('dotenv').config();
+
+const express = require('express');
+const mongoose = require('mongoose');
+
+//-- Prevent NoSQL injection
+const mongoSanitize = require('express-mongo-sanitize');
+
+const routes = require('./routes/index');
+
+const app = express();
+app.use(express.json());
+app.use(routes);
+
+//-- Prevent NoSQL injection
+app.use(
+  mongoSanitize({
+    onSanitize: ({ req, key }) => {
+      //-- Throw an error
+      //-- Catch with express error handler
+    }
+  })
+);
+
+mongoose
+  .connect(process.env.MONGODB_URI)
+  .then(() => {
+    console.log('Mongoose connected 🍃');
+    app.listen(3000, () => {
+      console.log('Server is up and running 🚀');
+    });
+  })
+  .catch((error) => {
+    console.log(error);
+  });
